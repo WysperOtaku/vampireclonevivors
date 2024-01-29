9b21a0bdd09d0d4dfc8baaf532851853e1236baa
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2024-01-29 17:18:52 +0100

    Prototipo Base juego 2
    Creacio de movimiento de enemigo nivel 1
    Creacion de vida player y enemigo nivel 1

---
+++ vampireclonevivorsgame/Assets/Scripts/Enemy N1.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyN1 : MonoBehaviour
+{
+    private float velocidadBala = 2.5f;
+    private GameObject player;
+    private int vidaEn1 = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BalaP"))
+        {
+            vidaEn1--;
+            if (vidaEn1 <= 0)
+            {
+            Destroy(this.gameObject);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Vector3 direccion = (player.transform.position - transform.position).normalized;
+            GetComponent<Rigidbody2D>().velocity = direccion * velocidadBala;
+        }
+    }
+}
